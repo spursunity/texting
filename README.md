15ab639809e493b96c53050bb874b0ce9485c815
spursunity <spursunity@mail.ru>
2019-05-11 11:59:29 +0300

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Test project
-
-## Name: Texting
-
-Now, this project is in progress.
-
-(instruments: Redux, Thunk, Firebase, Router-DOM)
---
+++ precache-manifest.7a83046976e8f56b059a2df885f15ce8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d71ce15d5f631a45e735",
+    "url": "/texting/static/css/main.20d62619.chunk.css"
+  },
+  {
+    "revision": "d71ce15d5f631a45e735",
+    "url": "/texting/static/js/main.382b70fd.chunk.js"
+  },
+  {
+    "revision": "3480ef8901a44f05456d",
+    "url": "/texting/static/js/runtime~main.4970d5dc.js"
+  },
+  {
+    "revision": "4d454127b7ad91847807",
+    "url": "/texting/static/js/2.fb186a55.chunk.js"
+  },
+  {
+    "revision": "b8852d81475ddaa516d298c4a79c497e",
+    "url": "/texting/index.html"
+  }
+];
